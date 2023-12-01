2cf0f977e07bf1b73a8cc21d1a6c7aeaf4eef4de
AnnaSzab <szablankaa@gmail.com>
2023-12-01 18:28:45 +0100

    Zadanie dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,13 +1,14 @@
 ﻿string name = "Ewa";
 var age = 33;
-string gender = "mezczyzna";
-if (age < 30 && gender == "kobieta")
+bool Women = true;
+if (age < 30 && Women)
     Console.WriteLine("Kobieta ponizej 30 lat");
-else if (age == 33 && gender == "kobieta")
+else if (age == 33 && Women && name == "Ewa")
     Console.WriteLine("Ewa, lat 33");
-else if (age < 18 && gender == "mezczyzna")
+else if (age < 18 && !Women)
     Console.WriteLine("Niepelnoletni mezczyzna");
-else Console.WriteLine("Niewiadoma");
+
+else Console.WriteLine("Za malo danych");
 
 
 
