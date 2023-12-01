5dfda165459c3c0755650e9ef052c2f2c1168681
AnnaSzab <szablankaa@gmail.com>
2023-12-01 15:31:18 +0100

    Zadanie dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+var age = 33;
+string gender = "mezczyzna";
+if (age < 30 && gender == "kobieta")
+    Console.WriteLine("Kobieta ponizej 30 lat");
+else if (age == 33 && gender == "kobieta")
+    Console.WriteLine("Ewa, lat 33");
+else if (age < 18 && gender == "mezczyzna")
+    Console.WriteLine("Niepelnoletni mezczyzna");
+else Console.WriteLine("Niewiadoma");
+
+
+
+
+
+
