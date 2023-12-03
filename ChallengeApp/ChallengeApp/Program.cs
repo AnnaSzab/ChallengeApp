1f44079cf09cb697836d7ef483a19add66208cdb
AnnaSzab <szablankaa@gmail.com>
2023-12-03 23:30:35 +0100

    Dzien 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,17 +1,21 @@
-﻿string name = "Ewa";
-var age = 33;
-bool Women = true;
-if (age < 30 && Women)
-    Console.WriteLine("Kobieta ponizej 30 lat");
-else if (age == 33 && Women && name == "Ewa")
-    Console.WriteLine("Ewa, lat 33");
-else if (age < 18 && !Women)
-    Console.WriteLine("Niepelnoletni mezczyzna");
-
-else Console.WriteLine("Za malo danych");
-
-
-
-
-
+﻿var number = 12345655440;
+string numberAsstring = number.ToString();
+char[] letters = numberAsstring.ToArray();
+char[] Check = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+int[] counters = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+for (int i = 0; i < letters.Length; i++)
+{
+    for (int j = 0; j < Check.Length; j++)
+    {
+        if (letters[i] == Check[j])
+        {
+            counters[j]++;
+        }
+    }
+}
+Console.Write("Wyniki dla liczby "); Console.WriteLine(number);
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("liczba "); Console.Write(i); Console.Write(" jest "); Console.WriteLine(counters[i]);
+}
 
